5c820c7e332d9058e8066576b956bfeb9d16241d
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-08-21 21:18:11 +0300

    Add files via upload

---
+++ 2  day.py
@@ -0,0 +1,21 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #hello
+>>> print("helo")
+helo
+>>> #hi
+>>> #my
+>>> #any thing
+>>> print("i like me")
+i like me
+>>> #print("hafsa")
+>>> print("hafsazaid")
+hafsazaid
+>>> """"
+iam hafsa alshamiri mmmm
+just tell you my
+name,
+""""
+print("I know your name ")
+SyntaxError: EOL while scanning string literal
+>>> 
