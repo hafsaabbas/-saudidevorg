cb10e03568af1cb8411907e583c738ac8e2422fa
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-08-25 20:56:56 +0300

    Add files via upload

---
+++ 6 day.py
@@ -0,0 +1,32 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=int(1)
+>>> y=int(2.8)
+>>> z=int("3")
+>>> print(x)
+1
+>>> print(y)
+2
+>>> print(z)
+3
+>>> x=float(1)
+>>> y=float(2.8)
+>>> z=float("3")
+>>> print(x)
+1.0
+>>> print(y)
+2.8
+>>> print(y)
+2.8
+>>> print(z)
+3.0
+>>> x=str("sl")
+>>> y=str(2)
+>>> z=str(3.0)
+>>> print(x)
+sl
+>>> print(y)
+2
+>>> print(z)
+3.0
+>>> 
