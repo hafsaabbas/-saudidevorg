2c58554c3cdc044340d11de77913bf59b027dcd2
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-02 12:51:38 +0300

    Add files via upload

---
+++ 14 day.py
@@ -0,0 +1,27 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> t=["apple","orange","panan"]
+>>> print(t[0:2])
+['apple', 'orange']
+>>> if "apple" in t :
+	print(yes)
+
+	
+Traceback (most recent call last):
+  File "<pyshell#4>", line 2, in <module>
+    print(yes)
+NameError: name 'yes' is not defined
+>>> if "apple" in t:
+	print(yes)
+
+	
+Traceback (most recent call last):
+  File "<pyshell#6>", line 2, in <module>
+    print(yes)
+NameError: name 'yes' is not defined
+>>> if "apple" in t:
+	print("yes")
+
+	
+yes
+>>> 
