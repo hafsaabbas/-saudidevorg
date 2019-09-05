eb05eea122dbef6b9b935370683718bd16d59b5e
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-06 01:54:24 +0300

    Add files via upload

---
+++ 18&19 day.py
@@ -0,0 +1,29 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> h=["milan","roma","italy"]
+>>> print(h[2])
+italy
+>>> h.append("sa")
+>>> print(h)
+['milan', 'roma', 'italy', 'sa']
+>>> h.pop()
+'sa'
+>>> print(h)
+['milan', 'roma', 'italy']
+>>> h.clear()
+>>> print(h)
+[]
+>>> s=("java","pyhton""swift")
+>>> if "python" in s:
+	print("yes")
+
+	
+>>> 
+>>> 
+>>> ha =("java","python","swift")
+>>> if "python" in ha:
+	print("yea,")
+
+	
+yea,
+>>> 
