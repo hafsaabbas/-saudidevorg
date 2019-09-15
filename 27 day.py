35042c4d651892260fb223fd7d2c1159c42edfae
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-15 23:34:48 +0300

    Add files via upload

---
+++ 27 day.py
@@ -0,0 +1,51 @@
+a=33
+b=200
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a is greater than b")
+else:
+    print("a is equal b")
+    
+a=33
+b=200
+if b>a:
+    print("b is greater than a")
+
+else:
+    print("a is equal b")
+a=33
+b=200
+if b>a : print("b is greater than a")
+
+x=2
+z=330
+print("a") if a>b else print ("b")
+
+x=330
+z=330
+print("a") if a>b else print ("=")if a==b else print("b")
+
+a=200
+b=33
+c=500
+if a>b and c>a:
+    print("both condithion are true")
+
+a=200
+b=33
+c=500
+if a>b or c>a:
+    print("both condithion are true")
+x=41
+if x>10:
+    print("above ten,")
+    if x>20 :
+        print("and also above 20!")
+    else :
+        print("but not above 20.")
+        
+    
+    
+
+ 
