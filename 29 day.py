1372c4102f157143e406572196e5f1c21d02e557
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-18 01:43:55 +0300

    Add files via upload

---
+++ 29 day.py
@@ -0,0 +1,30 @@
+f=["apple","banana","cherry"]
+for x in f:
+    print(x)
+
+for x in "banana":
+    print(X)
+    
+
+f=["apple","banana","cherry"]
+for x in f:
+     print(X)
+     if x =="banana":
+         break
+   
+   
+
+f=["apple","banana","cherry"]
+for x in f:
+    if x =="banana":
+        break
+    print(x)
+    
+ 
+f=["apple","banana","cherry"]
+for x in f:
+    if x =="banana":
+        continue
+    print(x)
+
+
