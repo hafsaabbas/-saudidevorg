1de3073a3fdb52630fe8f3c46ea3138b145be8c9
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-18 23:37:26 +0300

    Add files via upload

---
+++ 30 day.py
@@ -0,0 +1,16 @@
+for x in range(6):
+    print(x)
+
+for x in range(2,6):
+    print (x)
+
+for x in range(6):
+    print (x)
+else :
+    print("finaly")
+
+a=["red", "big","tasty"]
+f=["apple","babab","cherry"]
+for x in a:
+    for y in f:
+        print (x,y)
---
+++ 31 day.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("f")
+my_function()
+
+def m (name):
+    print(name +"mohanad")
+m("heesa")
+m("turkia")
+
+def m (name ="hafsa"):
+    print(name +"mohanad")
+m("heesa")
+m("turkia")
+m()
