19eed1fbb03c0b4bfae7da872782605b1e07b75e
Hafsa Zaid <33102766+hafsaabbas@users.noreply.github.com>
2019-09-27 04:46:47 +0300

    Add files via upload

---
+++ 37 day.py
@@ -0,0 +1,17 @@
+car=["fo","gh","gH"]
+print(car)
+
+car=["fo","gh","gH"]
+z=car[0]
+print(z)
+
+
+
+car=["fo","gh","gH"]
+car[0]="kj"
+print(car)
+
+car=["fo","gh","gH"]
+x=len(car)
+print(x)
+
---
+++ 38 day.py
@@ -0,0 +1,16 @@
+
+car=["fo","gh","gH"]
+for c in car:
+    print(c)
+
+car=["fo","gh","gH"]
+car.append("swq")
+print(car)
+
+car=["fo","gh","gH"]
+car.pop(1)
+print(car)
+
+car=["fo","gh","gH"]
+car.remove("fo")
+print(car)
